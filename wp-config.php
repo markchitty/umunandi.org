39da6f0a7cb3c0e05e262de26c59389aa0917b22
Mark Chitty <mark@calmfrog.com>
2014-02-24 16:11:44 +0000

    Production config settings in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -7,10 +7,10 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
 	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', '%%DB_NAME%%' );
-	define( 'DB_USER', '%%DB_USER%%' );
-	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
+	define( 'DB_NAME', 'myfriend_wp_umunandi' );
+	define( 'DB_USER', 'myfriend_wpuser' );
+	define( 'DB_PASSWORD', 'w]qr5Ia)B1Kt' );
+	define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 }
 
 // ========================
@@ -29,14 +29,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '|%tTe[-Xti?GLinoWEl.bO3Tef)zYs-!]|OT(JO!|TmLKu60F__q>M]rY$7H,}%+');
+define('SECURE_AUTH_KEY',  'e^#G%je{C*@d=a-fetL!-;Lu-RzOhrp_DLu=tJx<V@&ut(S&XZ}BnSL-V^.lO?{J');
+define('LOGGED_IN_KEY',    '-vA1i3pytm-+]9JmEut^5]pxoX-[H]s8^PmhPZM=T2z20oJmHv>lnJ4LKj`9Vw:A');
+define('NONCE_KEY',        'W1C<Xv:WssSni1C|H{B<QF+<vJyQL-lv|*-smTXX$v)m6Coiy5wT1wv!~H5;-/s&');
+define('AUTH_SALT',        'GGp+Ld-%C1t!!N5vYjiLzkVj%4!%:VMsG+x@Tqrq{$yxaMa2|yU+nV21i_qyrxmD');
+define('SECURE_AUTH_SALT', 'Bw~SnGs7:,$qG`Wiq&(su=crkeMy+U<Cohne.fu;_V3!gY2EBRB`M8l_~m<U}Twe');
+define('LOGGED_IN_SALT',   '|vct901v)ib|bM`HxT@3sQCfzEh!#e*Lnm?yN8qQlM#rUsxdGl1hRxP%<$ab/K[$');
+define('NONCE_SALT',       'SPA+vRV;P{o}OrUA>:pBncGGVb=i-ylouQ<&=#%xN%QlCJ.Fv^iTqouyl%iWYWt:');
 
 // ==============================================================
 // Table prefix
@@ -72,8 +72,8 @@ if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
 // ===========================================================================================
 // This can be used to programatically set the stage when deploying (e.g. production, staging)
 // ===========================================================================================
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
+// define( 'WP_STAGE', '%%WP_STAGE%%' );
+// define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
 
 // ===================
 // Bootstrap WordPress
