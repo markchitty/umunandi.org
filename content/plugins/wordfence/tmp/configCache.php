68ea8158a4d07c09807732b385a3c666dd5d714b
Mark Chitty <mark@calmfrog.com>
2014-08-04 12:54:57 +0100

    Remove syncdb-config from git - I'd kinda like it to be tracked but it ends up lying around in the production web root which is not good. Yeah, I could mop it up with a tweak to the syncdb script but leaving it out of git is a cleaner solution for now. Also ignore some Wordfence cache files that seem to change a lot.

--- content/plugins/wordfence/tmp/configCache.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Wordfence temporary file security header */
-echo "Nothing to see here!\n"; exit(0);
-?>a:12:{s:9:"cacheType";s:3:"php";s:17:"allowHTTPSCaching";s:1:"0";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:128:"dc07fe3a6a5eef48729686763c9f3df6a987b7a231911bda4d920ed2940942696789285727d2b8146c671552e9046a73b105392e59e284d2175ef55617ea383e";s:9:"howGetIPs";s:0:"";s:9:"IPGetFail";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:10:"tourClosed";s:1:"1";s:10:"autoUpdate";s:1:"1";s:11:"alertEmails";s:30:"mark+umunandi.dev@calmfrog.com";}
