3c8740f3ebf5c149ea3b61055c3ecdafde4c028b
Mark Chitty <mark@calmfrog.com>
2015-11-15 16:56:28 +0000

    Revise css - lots of changes in here that really ought to be tracked in individual commits, but too bad, cos they aint

--- content/themes/umunandi/lib/scripts.php
+++ content/themes/umunandi/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9a8cff6eab09b1b8e8c7287471c2fe47');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, 'aa602245f343e0947725ee16254a8a38');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
