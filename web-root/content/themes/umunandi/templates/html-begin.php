897a6f6c59613de0ba6ad73131f626cbfbc3ad4d
Mark Chitty <mark@calmfrog.com>
2016-01-11 09:36:28 +0000

    Favicon

--- web-root/content/themes/umunandi/templates/html-begin.php
+++ web-root/content/themes/umunandi/templates/html-begin.php
@@ -6,6 +6,18 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <?php
+  // Favicons
+  $favicon_path = get_template_directory_uri() . '/assets/img/favicon/';
+  ?>
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $favicon_path; ?>apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo $favicon_path; ?>apple-touch-icon-152x152.png" />
+  <link rel="icon" type="image/png" href="<?php echo $favicon_path; ?>favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="<?php echo $favicon_path; ?>favicon-16x16.png" sizes="16x16" />
+  <meta name="application-name" content="Umunandi | We help kids in Zambia get a better start in life"/>
+  <meta name="msapplication-TileColor" content="#CCCCCC" />
+  <meta name="msapplication-TileImage" content="<?php echo $favicon_path; ?>mstile-144x144.png" />
+
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
