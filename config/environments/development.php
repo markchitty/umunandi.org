0f6244ba30167d9b5817a450b7c5bfbfc37c49de
Mark Chitty <mark.chitty@lab49.com>
2017-05-03 14:32:56 +0100

    Only log errors, don't show them on-screen

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,6 +3,5 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
-
-// Enable Debug logging to the /wp-content/debug.log file
-define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_LOG', true );      // Log debug messages in /wp-content/debug.log
+define( 'WP_DEBUG_DISPLAY', false ); // Don't show debug messages in the html
