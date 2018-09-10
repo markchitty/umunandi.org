ab34bf0dea9b2031f790195e2788f774e70f060b
Mark Chitty <mark@calmfrog.com>
2018-09-10 12:20:37 +0100

    Add custom class name field to page admin screen

--- web/app/themes/umunandi/functions/admin.php
+++ web/app/themes/umunandi/functions/admin.php
@@ -17,8 +17,8 @@ function ulog() {
 }
 
 // Advanced Custom Forms styles
-add_action('acf/input/admin_head', 'my_acf_admin_head');
-function my_acf_admin_head() {
+add_action('acf/input/admin_head', 'umunandi_acf_admin_head');
+function umunandi_acf_admin_head() {
   ?>
   <style type="text/css">
     .acf_postbox.no_box > .hndle { display: none; }
@@ -27,6 +27,30 @@ function my_acf_admin_head() {
   <?php
 }
 
+// Custom page attribute field
+add_action('page_attributes_misc_attributes', 'umunandi_page_class');
+function umunandi_page_class($post) {
+  $page_class = get_post_meta($post->ID, 'umunandi_page_class', true);
+  wp_nonce_field('umunandi_meta_box_nonce', 'meta_box_nonce');
+  ?>
+  <p class="post-attributes-label-wrapper">
+    <label class="post-attributes-label" for="umunandi_page_class">Class</label>
+  </p>
+  <input type="text" name="umunandi_page_class" id="umunandi_page_class" value="<?= $page_class ?>">
+  <?php
+}
+
+add_action('save_post', 'umunandi_meta_box_save');
+function umunandi_meta_box_save($post_id) {
+  // Bail if auto save, user can't edit this post, or invalid/missing nonce
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+  if (!current_user_can('edit_post', $post_id)) return;
+  if (!isset($_POST['meta_box_nonce']) ||
+      !wp_verify_nonce($_POST['meta_box_nonce'], 'umunandi_meta_box_nonce')) return;
+
+  if (isset($_POST['umunandi_page_class']))
+      update_post_meta($post_id, 'umunandi_page_class', esc_attr($_POST['umunandi_page_class']));
+}
 
 // Add Formats dropdown to TinyMCE editor (row 2), for custom styles
 // add_filter('mce_buttons_2', 'add_mce_formats');
