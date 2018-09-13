cff42393a422d676d55b45be9074063a1db24a38
Mark Chitty <mark@calmfrog.com>
2018-09-13 08:27:29 +0100

    CSS tidy up

--- web/app/themes/umunandi/lib/scripts.php
+++ web/app/themes/umunandi/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2f94e634c9910c19cfce8609b9182ae4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '085b0a6c7f98cb9f94f6376cdee28191');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
