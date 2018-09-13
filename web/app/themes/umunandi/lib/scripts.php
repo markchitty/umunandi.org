8296d8462b2d889ca228e7bf324f936fdb0184d2
Mark Chitty <mark@calmfrog.com>
2018-09-13 09:05:40 +0100

    Reorganise LESS files

--- web/app/themes/umunandi/lib/scripts.php
+++ web/app/themes/umunandi/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '085b0a6c7f98cb9f94f6376cdee28191');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3c1ada6c28c7fd0f84af327c3b465426');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
