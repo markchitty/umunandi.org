dd0a991f8a89aaa861c73f85c8ffe66b46bbf03c
Mark Chitty <mark@calmfrog.com>
2018-09-15 13:50:52 +0100

    Ditch sidebar. Add contact page styles.

--- web/app/themes/umunandi/lib/scripts.php
+++ web/app/themes/umunandi/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'af2073be499342502df85b2ee1fb2cd2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4ed06b271e78fe975fb316bb582accf7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- web/app/themes/umunandi/templates/page-body.php
+++ web/app/themes/umunandi/templates/page-body.php
@@ -1,11 +1,11 @@
 <div class="container">
-  <div class="row body-row equal-height-cols">
+  <div class="row body-row">
 
-    <aside class="sidebar col-sm-3 hidden-xs" role="complementary">
+    <!-- <aside class="sidebar col-sm-3 hidden-xs" role="complementary">
       <?php get_template_part('templates/layout/sidebar'); ?>
-    </aside>
+    </aside> -->
 
-    <main class="main col-xs-12 col-sm-9" role="main">
+    <main class="main col-xs-12" role="main">
       <?php while (have_posts()) : the_post(); ?>
         <?php the_content(); ?>
       <?php endwhile; ?>
