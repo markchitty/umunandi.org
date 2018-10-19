b0510d9618daad901d6f70f3e891074e4ccb3c7e
Mark Chitty <mark@calmfrog.com>
2018-10-19 11:25:35 +0100

    <ul> list styling, with funky hand sketched bullets - Replace non-semantic ul/li's with plain old divs

--- web/app/themes/umunandi/templates/pages/home/header.php
+++ web/app/themes/umunandi/templates/pages/home/header.php
@@ -9,20 +9,18 @@
 
   <?php global $OVCs; $featured_kids = $OVCs->featured_kids(); ?>
   <div class="featured-kids">
-    <ul>
     <?php while($featured_kids->have_posts()) : $featured_kids->the_post(); ?>
-      <li class="kid">
-        <a href=".body-top"
-          data-scrollto="750"
-          data-scrolloffset="10"
-          data-target=".js-kids-carousel"
-          data-slide-to="<?= $featured_kids->current_post ?>">
-          <div class="kid-face"><?= wp_get_attachment_image(get_field('head_shot'), 'thumbnail') ?></div>
-          <div class="kid-name"><?php the_field('first_name') ?></div>
-        </a>
-      </li>
+    <div class="kid">
+      <a href=".body-top"
+        data-scrollto="750"
+        data-scrolloffset="10"
+        data-target=".js-kids-carousel"
+        data-slide-to="<?= $featured_kids->current_post ?>">
+        <div class="kid-face"><?= wp_get_attachment_image(get_field('head_shot'), 'thumbnail') ?></div>
+        <div class="kid-name"><?php the_field('first_name') ?></div>
+      </a>
+    </div>
     <?php endwhile; wp_reset_postdata(); ?>
-    </ul>
   </div>
 
   <div class="scroll-down">
