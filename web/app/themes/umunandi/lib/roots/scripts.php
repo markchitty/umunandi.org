2034c9274023b1cc046bb295c9aa4f51146c4159
Mark Chitty <mark@calmfrog.com>
2018-10-19 12:06:50 +0100

    Standardise font sizing across the site (prob more instances still to mop up)

--- web/app/themes/umunandi/lib/roots/scripts.php
+++ web/app/themes/umunandi/lib/roots/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f341b45401ca800aa07748a7a5af1aa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fcf4154d7ef13e05b942f8e53067287c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
